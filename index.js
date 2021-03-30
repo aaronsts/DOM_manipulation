9b9f7be161429939d57649b724a9898135e38fdf
aaron <aaron.staes@gmail.com>
2021-03-30 11:26:27 +0200

    -Added the other elements via DOM manipulation

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const container = document.querySelector('#container');
 
-const redParagraph = document.createElement('div');
+const redParagraph = document.createElement('p');
 redParagraph.textContent = "Hey I'm red";
 redParagraph.style.color = 'red';
 
 container.appendChild(redParagraph);
+
+const blueHeader = document.createElement('h3');
+blueHeader.textContent = "I'm a blue h3";
+blueHeader.style.color = 'blue';
+
+container.appendChild(blueHeader);
+
+const pinkDiv = document.createElement('div');
+pinkDiv.style.border = '1px solid black';
+pinkDiv.style.backgroundColor = 'pink';
+
+const divHeader = document.createElement('h1');
+divHeader.textContent = "I'm in a div";
+const divPara = document.createElement('p');
+divPara.textContent = "ME TOO!";
+
+pinkDiv.appendChild(divHeader);
+pinkDiv.appendChild(divPara);
+
+container.appendChild(pinkDiv);
+
